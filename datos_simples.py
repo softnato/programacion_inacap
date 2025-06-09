39b4f86c9e35d6ccb93c690520297b28956f4764
softnato <renatoalvarez2106@gmail.com>
2025-06-09 19:49:36 -0400

    clase_1_tipos_de_datos

---
+++ datos_simples.py
@@ -0,0 +1,28 @@
+cadena_de_texto_1 = 'cadena de texto'
+cadena_de_texto_2 = "otra cadena de texto"
+print ('esto es una cadena de texto')
+print ('esto es una cadena de texto con "comillas"')
+print("esto es una cadena de texto con 'comillas'")
+
+print(
+"""esta
+es      
+unaaaa 
+cadena de texto de multiples lineas
+""")
+
+#tipos_de_datos_numericos
+numero_entero = 49
+numero_decimal = 3.14
+
+#datos_de_tipo_logico
+#bool
+
+dato_verdadero = True
+
+dato_falso = False
+
+saludo = 'buen_dia'
+nombre = input("escriba su nombre:")
+texto_final = saludo + nombre
+print (texto_final)
