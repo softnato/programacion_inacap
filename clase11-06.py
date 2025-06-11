e1f0ec5d6a37be18dba7ab3eb4a22f9b082f0274
softnato <renatoalvarez@gmail.com>
2025-06-11 19:29:19 -0400

    datos compuestos

---
+++ clase11-06.py
@@ -0,0 +1,8 @@
+edad = int(input('Ingrese edad del participante'))
+if edad >= 18:
+    if edad <= 25:
+        print('el participante puede ingresar a la actividad')
+    else:
+        print('el participante no puede ingresar a la actividad')
+
+
