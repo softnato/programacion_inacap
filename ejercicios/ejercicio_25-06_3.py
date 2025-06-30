b597beda18fe1ab7dbffdb7d13477db14a64ebd5
softnato <renatoalvarez2106@gmail.com>
2025-06-30 19:51:33 -0400

    ejercicio 4

--- ejercicios/ejercicio_25-06_3.py
+++ ejercicios/ejercicio_25-06_3.py
@@ -2,7 +2,8 @@
 # calcular la suma de esa cantidad de numeros
 
 suma = 0
+
 for i in range (1,6) :
     num = float(input('ingrese numero'))
     suma += num
-print (f'{suma}')
+print (f'{suma}') 
---
+++ ejercicios/ejercicio_4.py
@@ -0,0 +1,6 @@
+#crear una funcion que permita convertir temperatura de kelvin a celcius
+# c° = k - 273.15
+
+kelvin = int(input('ingrese temperatura en grados kelvin:' ))
+print('su temperatura es:4')
+print(kelvin - 273.15)
